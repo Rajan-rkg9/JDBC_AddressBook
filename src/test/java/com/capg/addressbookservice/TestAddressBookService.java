ca6a35490fb7c07fc9cf949e0f17cafd5a5656ca
Rajan-rkg9 <amirajangupta@gmail.com>
2020-11-03 22:52:29 +0530

    [Rajan-rkg9] Updated. a contact detail and  checked if synced with DB

--- src/test/java/com/capg/addressbookservice/TestAddressBookService.java
+++ src/test/java/com/capg/addressbookservice/TestAddressBookService.java
@@ -1,12 +1,14 @@
 package com.capg.addressbookservice;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 
 
 public class TestAddressBookService {
@@ -23,6 +25,13 @@ public class TestAddressBookService {
 	public void givenAddressBookDB_WhenRetrieved_ShouldMatchContactsCount() throws DBServiceException{
 		contactsList = serviceObj.viewAddressBook();
 		assertEquals(6, contactsList.size());
+	}
+	
+	@Test
+	public void givenUpdatedContacts_WhenRetrieved_ShouldBeSyncedWithDB() throws DBServiceException{
+		serviceObj.updateContactDetails("West Bengal" , "822234" , "Sumit");
+		boolean isSynced = serviceObj.isAddressBookSyncedWithDB("Sumit");
+		assertTrue(isSynced);
 	}
 	
 }
